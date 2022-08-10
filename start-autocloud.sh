6bfd4e6b993d91f17214d345cd08b85962108b39
Antoine Martin <antoine@openance.com>
2022-08-10 05:53:10 +0000

    ✏️ Remove at variable coming from makefile

--- start-autocloud.sh
+++ start-autocloud.sh
@@ -23,7 +23,7 @@ set -ueo pipefail
 
 # Add the Kaweezle APK repository
 wget -qO - "https://github.com/kaweezle/iknite/releases/download/v0.1.18/kaweezle-devel@kaweezle.com-c9d89864.rsa.pub" > /etc/apk/keys/kaweezle-devel@kaweezle.com-c9d89864.rsa.pub
-grep -q kaweezle $@/etc/apk/repositories || echo https://kaweezle.com/repo/ >> $@/etc/apk/repositories 
+grep -q kaweezle /etc/apk/repositories || echo https://kaweezle.com/repo/ >> /etc/apk/repositories 
 
 # Add some minimal dependencies
 apk --update add krmfnsops k9s openssl iknite
